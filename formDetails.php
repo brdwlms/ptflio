4bb8d22870762a660c8f7ca7a593ba654cc781d3
Brad Williams <brdwlms@csu.fullerton.edu>
2019-12-15 12:15:55 -0800

    php :3

---
+++ formDetails.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+
+if(isset($_POST['Submit'])) {
+    $comments = $_POST['comments'];
+    $occupation = $_POST['occupation'];
+    echo "Thank you for your comment!" "<br/>";
+
+}
+    ?>
+</body>
+</html>
