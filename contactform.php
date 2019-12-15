b774ba19435e9aff7fa591376bcf70b2a32163e5
Brad Williams <brdwlms@csu.fullerton.edu>
2019-12-15 13:45:02 -0800

    Add files via upload

---
+++ contactform.php
@@ -0,0 +1,16 @@
+<?php
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $comments = $_POST['comments'];
+
+    $mailTo = "bradwilliamsee@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have recieved an Email from ".$name.".\n\n".$comments;
+
+
+    mail($mailTo, $comments, $txt, $headers);
+    header("Location: index.php?mailsend");
+
+}
+?>
